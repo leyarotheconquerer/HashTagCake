09febbada75036e32b11ee60b25a90298addc462
SpenSer Bray <matthewqa@gmail.com>
2015-04-18 18:24:36 -0500

    Add ability to jump.
    Also modify some initial values because my patch was angry. :(

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -2,13 +2,34 @@
 using System.Collections;
 
 public class PlayerMovementController : MonoBehaviour {
+	public bool inAir = true;
+
 	public float Speed = 4f;
 	public float Acceleration = 0.2f;
-	public float AlternateSpeedModifier = 4f;
+	public float AlternateSpeedModifier = 1.8f;
+
+	public float JumpForce = 250f;
+
+	public Transform GroundCheckPosition;
+	public float GroundCheckRadius = 0.04f;
+	public LayerMask GroundLayer;
 
 	protected bool facingRight = true;
 
+	public void Update() {
+		if(!inAir && Input.GetButtonDown("Jump")) {
+			rigidbody2D.AddForce(new Vector2(0f, JumpForce));
+		}
+	}
+
 	public void FixedUpdate() {
+		// Check to see if we're in air or grounded!
+		if(GroundCheckPosition)
+			inAir = !Physics2D.OverlapCircle(GroundCheckPosition.position, GroundCheckRadius, GroundLayer);
+		else
+			Debug.Log("ERROR: Ground Check Transform was not set.");
+
+		// Movement
 		float inputMovement = Input.GetAxis("Horizontal");
 		float calculatedSpeed = Speed*inputMovement*(Input.GetButton("Movement Modifier") ? AlternateSpeedModifier : 1f);
 
@@ -16,8 +37,6 @@ public class PlayerMovementController : MonoBehaviour {
 
 		if((inputMovement > 0 && !facingRight) || (inputMovement < 0 && facingRight))
 			FlipCharacter();
-
-		Debug.Log(calculatedSpeed);
 	}
 
 	private void FlipCharacter() {
