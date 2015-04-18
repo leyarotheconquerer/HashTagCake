2dfbb1611fea3abc9a10e5f1d5b8e173ba7d80c5
Sherlock95 <Nebilim95@gmail.com>
2015-04-18 14:57:34 -0500

    Commiting Generation of entrance, exit, and center points.

--- Assets/Scripts/LevelGeneration.cs
+++ Assets/Scripts/LevelGeneration.cs
@@ -5,16 +5,17 @@ public class LevelGeneration : MonoBehaviour {
 
 	public class Level {
 
-		private int sizeX, sizeY;
+		public int sizeX, sizeY;
 		public Block[][] world;
 
-		class Block {
+		public class Block {
 
-			public enum type {
+			public enum Type {
 				Wall,
 				Empty,
 				Door,
-				Exit
+				Exit,
+				Center
 			};
 
 			public int[][] tile;
@@ -27,12 +28,11 @@ public class LevelGeneration : MonoBehaviour {
 			public bool isWallLeft = false;
 			public bool isWallRight = false;
 			public bool isWall = true;
+			public Type type;
 
 			public Block() {
 				tile = new int[4][4];
-				foreach (int block in tile) {
-					block = type.Wall;
-				}
+				type = Type.Wall;
 			}
 		}
 
@@ -49,14 +49,46 @@ public class LevelGeneration : MonoBehaviour {
 		}
 	}
 
+	Level level;
+
 	// Use this for initialization
 	void Start () {
-
+		level = new Level (128, 128);
+		GenerateLevel (level);
 	}
 
 	// Actual level generation function
 	Level GenerateLevel(Level level) {
+		// Set Enterance
+		int enterX = (int)Random.Range (0, level.sizeX);
+		int enterY = (int)Random.Range (0, level.sizeY);
+		level.world [enterX] [enterY].type = Level.Block.Type.Door;
 
+		// Set Exit (x,y)
+		int exitX = (int)Random.Range (0, level.sizeX);
+		int exitY = (int)Random.Range (0, level.sizeY);
+
+		// If entrance (x,y) equals exit (x,y), get new (x,y) for exit
+		while (exitX == enterX && exitY == enterY) {
+			exitX = (int)Random.Range (0, level.sizeX);
+			exitY = (int)Random.Range (0, level.sizeY);
+		}
+
+		// Set Exit
+		level.world [exitX] [exitY].type = Level.Block.Type.Exit;
+
+		// Set the weighted "center" (x,y)
+		int centerX = (int)Random.Range (0, level.sizeX);
+		int centerY = (int)Random.Range (0, level.sizeY);
+
+		// Get new values if the center (x,y) equals either the entrance (x,y) or the exit(x,y)
+		while ((centerX == enterX && centerY == enterY) || (centerX == exitX && centerY == exitY)) {
+			centerX = (int)Random.Range (0, level.sizeX);
+			centerY = (int)Random.Range (0, level.sizeY);
+		}
+
+		// Set "center"
+		level.world [centerX] [centerY] = Level.Block.Type.Center;
 	}
 
 	// Update is called once per frame
