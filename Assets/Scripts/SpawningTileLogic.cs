dc008593f0ff06b54595f5ef7e96f462f7eddeed
Hazen Johnson <HazenJohnson@letu.edu>
2015-04-20 15:56:03 -0500

    Add spawning tiles.

---
+++ Assets/Scripts/SpawningTileLogic.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawningTileLogic : MonoBehaviour {
+
+	public static Dictionary<string, GameObject> Parent;
+	public string SceneParent = "DefaultParent";
+	public Transform SpawningLocation;
+
+	public List<GameObject> Spawnables;
+	
+	void Start () {
+		if(Parent == null)
+			Parent = new Dictionary<string, GameObject>();
+
+		if(!Parent.ContainsKey(SceneParent)) {
+			Parent[SceneParent] = new GameObject();
+			Parent[SceneParent].name = SceneParent;
+		}
+
+		int enemyId = Random.Range(0, Spawnables.Count);
+
+		GameObject enemy = (GameObject)Instantiate(Spawnables[enemyId], SpawningLocation.position, SpawningLocation.rotation);
+
+		enemy.transform.SetParent(Parent[SceneParent].transform);
+		enemy.transform.localScale = Vector3.one;
+	}
+}
