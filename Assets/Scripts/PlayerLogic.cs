b31e94bfede95f977cc3c56deb3cfb8c8ec70455
leyarotheconquerer <HazenJohnson@letu.edu>
2015-04-19 23:18:46 -0500

    Correct some prefab errors introduced in a merge.
    Sorry... meta file problems incoming.

--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -4,26 +4,10 @@ using System.Collections;
 public class PlayerLogic : MonoBehaviour {
 
 	public static GameObject player = null;
-	public LayerMask layerMask;
 
 	// Use this for initialization
 	void Start () 
 	{
 		DontDestroyOnLoad(transform.gameObject);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	void OnCollisionEnter2D(Collider2D collision)
-	{
-		Debug.Log ("collision");
-		if ((layerMask.value & (1<<collider.gameObject.layer)) > 0) 
-		{
-			this.GetComponent<Unit>().AddWeapon(collision.gameObject);
-			Debug.Log("player picked up weapon");
-		}
 	}
 }
