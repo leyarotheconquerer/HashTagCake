50dbf89fbe313d7b3f92f0295a03c840e60a9e87
SpenSer Bray <matthewqa@gmail.com>
2015-04-19 23:37:18 -0500

    Setup all the menus and scenes for menus.

---
+++ Assets/Scripts/Menu/GotoScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GotoScene : MonoBehaviour {
+	public string SceneName;
+
+	public void ChangeScene() {
+		Application.LoadLevel(SceneName);
+	}
+}
