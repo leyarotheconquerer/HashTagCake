e85164c887955c3d1258efba94c1029c295f8dc0
SpenSer Bray <matthewqa@gmail.com>
2015-04-20 02:32:17 -0500

    Lolgotosceneisdumb.

--- Assets/Scripts/Menu/GotoScene.cs
+++ Assets/Scripts/Menu/GotoScene.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class GotoScene : MonoBehaviour {
 	public string SceneName;
+	public bool ResetPlayer = false; // Not a hack. :)
 
 	public void ChangeScene() {
 		Application.LoadLevel(SceneName);
+
+		if(ResetPlayer)
+			PlayerLogic.player = null;
 	}
 }
