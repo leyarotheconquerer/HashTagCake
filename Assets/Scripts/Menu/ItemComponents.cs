91b7100eec89a127a00a1acb51ddf1cf21818d16
Hazen Johnson <HazenJohnson@letu.edu>
2015-04-20 20:07:05 -0500

    Begin initial work on menu.

---
+++ Assets/Scripts/Menu/ItemComponents.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class ItemComponent {
+	public string Name;
+	public GameObject Child;
+}
+
+public class ItemComponents : MonoBehaviour {
+
+	public List<ItemComponent> Components;
+
+	public Dictionary<string, GameObject> ComponentDictionary;
+
+	void Start() {
+		foreach(ItemComponent component in Components) {
+
+		}
+	}
+}
---
+++ Assets/Scripts/Menu/PickupMenu.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PickupMenu : MonoBehaviour {
+	
+	public static GameObject Weapon = null;
+	public Unit Unit;
+
+	public GameObject ArmorItem;
+	public GameObject StrengthItem;
+
+	public Text Health;
+
+	public GameObject armorList;
+	public GameObject currentStrengthList;
+	public GameObject newStrengthList;
+
+	void Awake() {
+		Unit = PlayerLogic.player.GetComponent<Unit>();
+		if(!Unit)
+			Debug.LogWarning("Can't find the Unit component of the player");
+
+		Health.text = "Health: " + Unit.Health + "/" + Unit.MaxHealth;
+
+		foreach(string element in Unit.Armor.Keys) {
+			GameObject armorItem = (GameObject)Instantiate(ArmorItem);
+			ItemComponents armorComponents = armorItem.GetComponent<ItemComponents>();
+		}
+	}
+
+	void AddWeapon() {
+		if(Weapon)
+			Unit.AddWeapon(Weapon);
+	}
+
+	void ReplaceWeapon() {
+		if(Weapon) {
+			Weapon oldWeapon = Unit.ReplaceWeapon(Weapon);
+
+			if(oldWeapon) {
+				oldWeapon.DestroySubWeapons();
+				Destroy(oldWeapon.gameObject);
+			}
+		}
+	}
+}
